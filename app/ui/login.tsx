00c0303eb997aae9fc235fd7fb21833861ccb47e
salvatore <salvatore.tosich.dev@gmail.com>
2024-09-16 17:21:48 +0200

    aggiusto show in from login e signup

--- app/ui/login.tsx
+++ app/ui/login.tsx
@@ -29,8 +29,8 @@ function Login() {
                                 {state?.errors?.email && <p>{state.errors.email}</p>}
                             </div>
                             <div>
-                                <label htmlFor="password" className="flex items-center justify-between mb-2 text-sm font-medium text-gray-900 ">Password
-                                    <button type='button' onClick={() => setShowPassword(prev => !prev)}>{!showPassword ?
+                                <label htmlFor="password" className=" relative flex items-center justify-between mb-2 text-sm font-medium text-gray-900 ">Password
+                                    <button className=' absolute top-10 right-2' type='button' onClick={() => setShowPassword(prev => !prev)}>{!showPassword ?
                                         <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" strokeWidth="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
                                             <path stroke="currentColor" strokeWidth="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
